b8ea1892c8f9e9b72ac5ac5dedd8810a022d6237
nitor <brett.mulligan@gmail.com>
2014-06-06 18:05:00 -0500

    Put all input checking in a new function.
    Increases encapsulation.

--- pybox.py
+++ pybox.py
@@ -8,6 +8,17 @@ from pygame.locals import *
 
 res = (1366, 768) # resolution
 
+coords1 = (60, 160)
+coords2 = (120, 60)
+thick = 4
+
+scrollSpeed = 3
+
+mousex, mousey = 0, 0
+
+
+# setup
+
 pygame.init()
 fpsClock = pygame.time.Clock()
 
@@ -20,31 +31,14 @@ blue = pygame.Color(0,0,255)
 white = pygame.Color(255,255,255)
 black = pygame.Color(0,0,0)
 
-mousex, mousey = 0, 0
-
 fontObj = pygame.font.Font('freesansbold.ttf', 16)
 msg = 'Hello pygame!'
 
-coords1 = (60, 160)
-coords2 = (120, 60)
-thick = 4
 
-scrollSpeed = 3
 
-# main program loop
-while True:
-
-    # draw
-    windowSurfObj.fill(white)
+def processInput():
+    global coords1, coords2, msg, mousex, mousey
     
-    pygame.draw.line(windowSurfObj, blue, (mousex, mousey), coords2, thick)
-    
-    msgSurfObj = fontObj.render(msg, False, black)
-    msgRectObj = msgSurfObj.get_rect()
-    msgRectObj.topleft = (res[0]/4,20)
-    windowSurfObj.blit(msgSurfObj, msgRectObj)
-
-    # process input
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -70,10 +64,31 @@ while True:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
+
+
+# main program loop
+while True:
+
+    # draw
+    windowSurfObj.fill(white)
+    
+    pygame.draw.line(windowSurfObj, blue, (mousex, mousey), coords2, thick)
+    
+    msgSurfObj = fontObj.render(msg, False, black)
+    msgRectObj = msgSurfObj.get_rect()
+    msgRectObj.topleft = (res[0]/4,20)
+    windowSurfObj.blit(msgSurfObj, msgRectObj)
+
+    
         
-    else:
-        pass
+    processInput()
 
     # update draw
     pygame.display.update()
     fpsClock.tick(30)
+
+    
+
+    
+def draw ():
+    pass
